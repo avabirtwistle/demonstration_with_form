6fc74949f168a2119ee47c40b8f4c8fdc8d5aadb
“avabirtwistle” <evebirt@gmail.com>
2025-06-18 16:14:09 -0400

    started the non-prototype java backend, configured dependencies, added intial file structure

---
+++ backend/java/src/main/java/com/greenreach/RootController.java
@@ -0,0 +1,13 @@
+package com.greenreach;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Backend is alive.";
+    }
+}
